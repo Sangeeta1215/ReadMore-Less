9013063d4fc9e78087f9da72a4fa0e1c636467eb
Sangeeta Devi <sd705194@gmail.com>
2023-05-25 00:22:53 +0530

    Initial commit

---
+++ readMore.js
@@ -0,0 +1,17 @@
+var dots=document.getElementById("dots");
+var more=document.getElementById("more");
+var btn1=document.getElementById("btn1");
+function readMore()
+{
+if(dots.style.display==="none") 
+{
+dots.style.display="inline";
+btn1.innerHTML="Read more";
+more.style.display="none";
+}
+else{
+dots.style.display="none";
+btn1.innerHTML="Read less";
+more.style.display="inline";
+}
+}
